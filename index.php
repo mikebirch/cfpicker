3ca389fadc8ac25ea35c86bdeb17b0c2a8a75312
Michael Birch <mbirch321@gmail.com>
2014-08-15 10:58:13 +1200

    Added more tones which are calculated by Sass tint and shade functions New layout for index.php inspired by 0to255.com

--- index.php
+++ index.php
@@ -7,11 +7,25 @@ $base_colours = array(
 );
 
 $tones = array(
-  'lightest',   
-  'lighter',    
+  'light90',
+  'light80', 
+  'light70',  
+  'light60',
+  'light50',
+  'light40',
+  'light30',
+  'light20',  
+  'light10',    
   'base',            
-  'darker',     
-  'darkest',
+  'dark10',     
+  'dark20',
+  'dark30',
+  'dark40',
+  'dark50',
+  'dark60',
+  'dark70',
+  'dark80',
+  'dark90',
 );
 ?>
 <!DOCTYPE html>
@@ -27,17 +41,27 @@ $tones = array(
   <p>A handy colour picker for designing in the browser with Sass. Clicking on a colour selects the text so that you can copy and paste it into your editor.</p>
   <p>Read the associated <a href="/blog/colour-picker-for-sass">blog post</a>.</p>
   <p>View on <a href="https://github.com/mikebirch/cfpicker">GitHub</a>.</p>
-  <div class="wrapper">
-  <?php foreach ($base_colours as $colour) : ?><?php foreach ($tones as $tone) : ?><div class="box <?php echo $colour.'-'.$tone ?>"><textarea>colour(<?php echo $colour ?>,<?php echo $tone ?>)</textarea></div><?php endforeach ?><?php endforeach ?>
-</div>
-<script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
-<script>window.jQuery || document.write('<script src="js/jquery-2.1.1.min.js"><\/script>')</script>
-<script>
-  jQuery(document).ready(function($) {
-    $('.box').click(function() {
-      $(this).children('textarea').select();
+
+    <?php foreach ($base_colours as $colour) : ?>
+    <div class="col">
+      <?php foreach ($tones as $tone) : ?>
+      <div class="box <?php echo $colour.'-'.$tone ?>">
+        <textarea>colour(<?php echo $colour ?>,<?php echo $tone ?>)</textarea>
+      </div>
+      <?php endforeach ?>
+    </div>
+    <?php endforeach ?> 
+
+  <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+  <script>window.jQuery || document.write('<script src="js/jquery-2.1.1.min.js"><\/script>')</script>
+  <script>
+    jQuery(document).ready(function($) {
+      $('.box').click(function() {
+        $('.active').removeClass('active');
+        $(this).addClass('active');
+        $(this).children('textarea').select();
+      });
     });
-  });
   </script>
 </body>
 </html>
